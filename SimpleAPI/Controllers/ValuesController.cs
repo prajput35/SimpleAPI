aabc424c68ab759b83b47bd2bd8109cbba8d8314
Paras <Paras.code.api@gmail.com>
2020-06-26 16:37:58 +0530

    Broken Unit Test :(

--- SimpleAPI/Controllers/ValuesController.cs
+++ SimpleAPI/Controllers/ValuesController.cs
@@ -22,7 +22,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "Paras Rajput";
+            return "Deepti Rajput";
         }
 
         // POST api/values
